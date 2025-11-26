d2d05ead52779c4a20ed334637bf69d6ae5278bb
Ali Hamoudi <hamoudiali1415@gmail.com>
2025-11-26 00:57:37 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import logoUrl from "./assets/logo.png";
+import { Card } from "./card";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className=" min-w-screen min-h-screen bg-background text-foreground flex items-center justify-center display-flex flex-col gap-4">
+      <div className="flex flex-row items-center justify-center gap-10">
+        <img
+          src={logoUrl}
+          alt="Viral Engine Logo"
+          className="justify-content-center mb-8 h-32 w-32 animate-fade-in"
+        />
+        <div className="text-6xl font-semibold mb-6">Viral Engine</div>
+      </div>
+      <div className="text-xl">Coming Soon...</div>
+    </div>
+  );
+}
+
+export default App;
